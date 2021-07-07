7e416dfdbb44666868710e37a1dd581186b1fa82
Henry Lee <henryleeworld@gmail.com>
2021-07-08 07:30:33 +0800

    feat:更新最新資料

--- js/preschools-map.js
+++ js/preschools-map.js
@@ -14,9 +14,20 @@ for (var z = 0; z < 20; ++z) {
     matrixIds[z] = z;
 }
 
+var cityList = {};
+var filterCity = '',
+    filterTown = '';
+var filterExtent = false;
+
 function pointStyleFunction(f) {
     var p = f.getProperties(),
         color, stroke, radius;
+    if (filterCity !== '' && p.city !== filterCity) {
+        return null;
+    }
+    if (filterTown !== '' && p.town !== filterTown) {
+        return null;
+    }
     if (f === currentFeature) {
         stroke = new ol.style.Stroke({
             color: '#000',
@@ -24,11 +35,19 @@ function pointStyleFunction(f) {
         });
         radius = 25;
     } else {
-        stroke = new ol.style.Stroke({
-            color: '#fff',
-            width: 2
-        });
-        radius = 15;
+        if (p.penalty === '有') {
+            stroke = new ol.style.Stroke({
+                color: '#f00',
+                width: 2
+            });
+        } else {
+            stroke = new ol.style.Stroke({
+                color: '#fff',
+                width: 2
+            });
+        }
+
+        radius = 20;
     }
     switch (p.type) {
         case '公立':
@@ -45,7 +64,7 @@ function pointStyleFunction(f) {
             color = '#ffdd57';
             break;
     }
-    return new ol.style.Style({
+    let pointStyle = new ol.style.Style({
         image: new ol.style.RegularShape({
             radius: radius,
             points: 3,
@@ -53,24 +72,102 @@ function pointStyleFunction(f) {
                 color: color
             }),
             stroke: stroke
+        }),
+        text: new ol.style.Text({
+            font: '14px "Open Sans", "Arial Unicode MS", "sans-serif"',
+            fill: new ol.style.Fill({
+                color: 'rgba(0,0,255,0.7)'
+            })
         })
-    })
+    });
+    pointStyle.getText().setText(p.monthly.toString());
+    return pointStyle;
 }
 var sidebarTitle = document.getElementById('sidebarTitle');
-var content = document.getElementById('sidebarContent');
+var content = document.getElementById('infoBox');
+var slip = document.getElementById('slipBox');
+var slip109 = document.getElementById('slipBox109');
 
 var appView = new ol.View({
     center: ol.proj.fromLonLat([120.221507, 23.000694]),
     zoom: 14
 });
 
+var vectorSource = new ol.source.Vector({
+    url: 'data/collection.json',
+    format: new ol.format.GeoJSON({
+        featureProjection: appView.getProjection()
+    })
+});
+var cityOptionDone = false;
+vectorSource.on('change', function() {
+    if (vectorSource.getState() == 'ready') {
+        vectorSource.forEachFeature(function(f) {
+            var p = f.getProperties();
+            if (false === cityOptionDone) {
+                if (!cityList[p.city]) {
+                    cityList[p.city] = {};
+                }
+                if (!cityList[p.city][p.town]) {
+                    cityList[p.city][p.town] = 0;
+                }
+                ++cityList[p.city][p.town];
+            }
+            if (filterTown !== '') {
+                if (p.city === filterCity && p.town === filterTown) {
+                    if (false === filterExtent) {
+                        filterExtent = f.getGeometry().getExtent();
+                    } else {
+                        ol.extent.extend(filterExtent, f.getGeometry().getExtent());
+                    }
+                }
+            } else if (filterCity !== '') {
+                if (p.city === filterCity) {
+                    if (false === filterExtent) {
+                        filterExtent = f.getGeometry().getExtent();
+                    } else {
+                        ol.extent.extend(filterExtent, f.getGeometry().getExtent());
+                    }
+                }
+            }
+
+        });
+        if (false === cityOptionDone) {
+            cityOptionDone = true;
+            var cityOptions = '<option value="">--</option>';
+            for (city in cityList) {
+                cityOptions += '<option>' + city + '</option>';
+            }
+            $('select#city').html(cityOptions);
+        }
+        if (false !== filterExtent) {
+            map.getView().fit(filterExtent);
+            filterExtent = false;
+        }
+    }
+});
+
+$('select#city').change(function() {
+    filterCity = $(this).val();
+    var townOptions = '<option value="">--</option>';
+    if (filterCity !== '') {
+        for (town in cityList[filterCity]) {
+            townOptions += '<option>' + town + '</option>';
+        }
+    }
+    $('select#town').html(townOptions);
+    filterTown = '';
+    filterExtent = false;
+    vectorSource.refresh();
+});
+$('select#town').change(function() {
+    filterTown = $(this).val();
+    filterExtent = false;
+    vectorSource.refresh();
+});
+
 var vectorPoints = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: 'data/collection.json',
-        format: new ol.format.GeoJSON({
-            featureProjection: appView.getProjection()
-        })
-    }),
+    source: vectorSource,
     style: pointStyleFunction
 });
 
@@ -116,7 +213,7 @@ map.on('singleclick', function(evt) {
             message += '<tr><th scope="row">住址</th><td>' + p.city + p.town + p.address + '</td></tr>';
             message += '<tr><th scope="row">類型</th><td>' + p.type + '</td></tr>';
             message += '<tr><th scope="row">核定人數</th><td>' + p.count_approved + '</td></tr>';
-            message += '<tr><th scope="row">五歲免費</th><td>' + p.free5 + '</td></tr>';
+            message += '<tr><th scope="row">五歲免費</th><td>' + p.is_free5 + '</td></tr>';
             message += '<tr><th scope="row">準公共化</th><td>' + p.pre_public + '</td></tr>';
             if (p.url !== '') {
                 message += '<tr><th scope="row">網址</th><td><a href="' + p.url + '" target="_blank">' + p.url + '</a></td></tr>';
@@ -132,6 +229,129 @@ map.on('singleclick', function(evt) {
             message += '</tbody></table>';
             sidebarTitle.innerHTML = p.title;
             content.innerHTML = message;
+
+            if (p.penalty === '有') {
+                $.getJSON('data/punish/' + p.city + '/' + p.title + '.json', {}, function(r) {
+                    var message = '';
+                    for (let line of r) {
+                        message += '<table class="table table-dark"><tbody>';
+                        message += '<tr><td>' + line[0] + '</td></tr>';
+                        message += '<tr><td>' + line[1] + '</td></tr>';
+                        message += '<tr><td>' + line[2] + '</td></tr>';
+                        message += '<tr><td>' + line[3] + '</td></tr>';
+                        message += '<tr><td>' + line[4] + '</td></tr>';
+                        message += '<tr><td>' + line[5] + '</td></tr>';
+                        message += '</tbody></table>';
+                    }
+                    $('#punishmentBox').html(message);
+                });
+                $('#punishmentItem').show();
+            } else {
+                $('#punishmentBox').html('');
+                $('#punishmentItem').hide();
+            }
+
+            $('#accordion110').hide();
+            $.getJSON('data/slip/' + p.city + '/' + p.title + '.json', {}, function(r) {
+                var message = '<table class="table table-dark">';
+                message += '<tbody>';
+                let slipKeys = ['學費', '雜費', '材料費', '活動費', '午餐費', '點心費', '全學期總收費', '交通費', '課後延托費', '家長會費'];
+                for (y in r.slip) {
+                    for (p in r.slip[y]) {
+                        message += '<tr><td colspan="2">';
+                        message += y + '歲 - ' + p + ' / ' + r.slip[y][p].months + '個月';
+                        message += '</td></tr>';
+                        let blockToShow = false;
+                        for (let slipKey of slipKeys) {
+                            if (r.slip[y][p].class['全日班'][slipKey] && r.slip[y][p].class['全日班'][slipKey]['單價'] != '') {
+                                blockToShow = true;
+                            }
+                        }
+                        if (blockToShow) {
+                            message += '<tr><td colspan="2" style="text-align:right;">全日班</td></tr>';
+                            message += '<tr><td colspan="2" class="table-responsive"><table class="table-dark" style="width:100%;">'
+                            message += '<tr><th>項目</th><th>收費期間</th><th>單價</th><th>小計</th></tr>';
+                            for (let slipKey of slipKeys) {
+                                if (r.slip[y][p].class['全日班'][slipKey]) {
+                                    message += '<tr><td>' + slipKey + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['收費期間'] + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['單價'] + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['小計'] + '</td></tr>';
+                                }
+                            }
+                            message += '</table></td></tr>';
+                        }
+                        blockToShow = false;
+                        for (let slipKey of slipKeys) {
+                            if (r.slip[y][p].class['半日班'][slipKey] && r.slip[y][p].class['半日班'][slipKey]['單價'] != '') {
+                                blockToShow = true;
+                            }
+                        }
+                        if (blockToShow) {
+                            message += '<tr><td colspan="2" style="text-align:right;">半日班</td></tr>';
+                            message += '<tr><td colspan="2" class="table-responsive"><table class="table-dark" style="width:100%;">'
+                            message += '<tr><th>項目</th><th>收費期間</th><th>單價</th><th>小計</th></tr>';
+                            for (let slipKey of slipKeys) {
+                                if (r.slip[y][p].class['半日班'][slipKey]) {
+                                    message += '<tr><td>' + slipKey + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['收費期間'] + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['單價'] + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['小計'] + '</td></tr>';
+                                }
+                            }
+                            message += '</table></td></tr>';
+                        }
+                    }
+                }
+                message += '</tbody></table>';
+                slip.innerHTML = message;
+                $('#accordion110').show();
+            });
+
+            $('#accordion109').hide();
+            $.getJSON('data/slip109/' + p.city + '/' + p.title + '.json', {}, function(r) {
+                var message = '<table class="table table-dark">';
+                message += '<tbody>';
+                let slipKeys = ['學費', '雜費', '材料費', '活動費', '午餐費', '點心費', '全學期總收費', '交通費', '課後延托費', '家長會費'];
+                for (y in r.slip) {
+                    for (p in r.slip[y]) {
+                        message += '<tr><td colspan="2">';
+                        message += y + '歲 - ' + p + ' / ' + r.slip[y][p].months + '個月';
+                        message += '</td></tr>';
+                        let blockToShow = false;
+                        for (let slipKey of slipKeys) {
+                            if (r.slip[y][p].class['全日班'][slipKey] && r.slip[y][p].class['全日班'][slipKey]['單價'] != '') {
+                                blockToShow = true;
+                            }
+                        }
+                        if (blockToShow) {
+                            message += '<tr><td colspan="2" style="text-align:right;">全日班</td></tr>';
+                            message += '<tr><td colspan="2" class="table-responsive"><table class="table-dark" style="width:100%;">'
+                            message += '<tr><th>項目</th><th>收費期間</th><th>單價</th><th>小計</th></tr>';
+                            for (let slipKey of slipKeys) {
+                                if (r.slip[y][p].class['全日班'][slipKey]) {
+                                    message += '<tr><td>' + slipKey + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['收費期間'] + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['單價'] + '</td><td>' + r.slip[y][p].class['全日班'][slipKey]['小計'] + '</td></tr>';
+                                }
+                            }
+                            message += '</table></td></tr>';
+                        }
+                        blockToShow = false;
+                        for (let slipKey of slipKeys) {
+                            if (r.slip[y][p].class['半日班'][slipKey] && r.slip[y][p].class['半日班'][slipKey]['單價'] != '') {
+                                blockToShow = true;
+                            }
+                        }
+                        if (blockToShow) {
+                            message += '<tr><td colspan="2" style="text-align:right;">半日班</td></tr>';
+                            message += '<tr><td colspan="2" class="table-responsive"><table class="table-dark" style="width:100%;">'
+                            message += '<tr><th>項目</th><th>收費期間</th><th>單價</th><th>小計</th></tr>';
+                            for (let slipKey of slipKeys) {
+                                if (r.slip[y][p].class['半日班'][slipKey]) {
+                                    message += '<tr><td>' + slipKey + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['收費期間'] + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['單價'] + '</td><td>' + r.slip[y][p].class['半日班'][slipKey]['小計'] + '</td></tr>';
+                                }
+                            }
+                            message += '</table></td></tr>';
+                        }
+                    }
+                }
+                message += '</tbody></table>';
+                slip109.innerHTML = message;
+                $('#accordion109').show();
+            });
             sidebar.open('home');
         }
     });
